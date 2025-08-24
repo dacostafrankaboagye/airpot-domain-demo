608feda4c16bc73eaa6a13b1ba5a51c81f9e20dc
dacostafrankaboagye <frank.aboagye@ashesi.edu.gh>
2025-08-24 08:00:26 +0000

    domain driven design

---
+++ src/main/java/com/example/airpot/AirpotDomainDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.airpot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirpotDomainDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirpotDomainDemoApplication.class, args);
+    }
+
+}
